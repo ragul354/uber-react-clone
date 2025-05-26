2bda3b6394672e38bbcc9e3057e5fa53c2cf1ceb
Ragul <rahul030504@gmail.com>
2025-05-26 08:24:52 -0700

    git push origin main Updated footer layout using Flexbox

--- src/footer.js
+++ src/footer.js
@@ -3,6 +3,38 @@ function Foot(){
         <div className="footer">
             <h2>Uber</h2>
             <p>Visit Help center</p>
+            <div className="footer-container">
+            <div>
+                <h3>Company</h3>
+                <p>About us</p>
+                <p>Our Offerings</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Blog</p>
+                <p>Careers</p>
+            </div>
+            <div>
+                <h3>Products</h3>
+                <p>Ride</p>
+                <p>Drive</p>
+                <p>Eat</p>
+                <p>Uber for Business</p>
+                <p>Uber for Fright</p>
+                <p>Gift Card</p>
+                <p>Uber Health</p>
+            </div>
+            <div>
+                <h3>Global Citizenship</h3>
+                <p>Saftey</p>
+                <p>Substainablity</p>
+            </div>
+            <div>
+                <h3>Travel</h3>
+                <p>reserve</p>
+                <p>Airport</p>
+                <p>Cities</p>
+            </div>
+            </div>
         </div>
     )
 }
